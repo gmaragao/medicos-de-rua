07b22fbaec35e51bfa76c51f9d41aa7c6701738c
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-08-02 15:30:10 -0300

    Using the correct token

--- src/app/config/verifyToken.js
+++ src/app/config/verifyToken.js
@@ -4,7 +4,7 @@ module.exports = function verify(req, res, next) {
   const token = req.header('auth-token');
   if (!token) return res.status(401).send('Acesso negado!');
   try {
-    const verified = jwt.verify(token, 'vascooooooooo');
+    const verified = jwt.verify(token, process.env.JWT_KEY);
     req.user = verified;
     next();
   } catch (err) {
