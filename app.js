8953a5a0b11fc7210ae604ace4774d65383c451d
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-08-02 17:24:26 -0300

    Exporting app and changing server to app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const config = require('./src/app/config/config');
+const mongoose = require('mongoose');
+const port = 3000;
+
+config(app, express, bodyParser, mongoose);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = app;
